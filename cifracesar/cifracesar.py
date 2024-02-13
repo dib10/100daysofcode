9423354d3aabe2e3c524ee6f69d32c267b575184
dib10 <caiodib10@gmail.com>
2024-02-13 17:10:48 -0300

    arrumando a lógica da função caesar

--- cifracesar/cifracesar.py
+++ cifracesar/cifracesar.py
@@ -4,17 +4,14 @@ direcao = input("Digite 'codificar' para criptografar, digite 'decodificar' para
 
 texto = input("Digite sua mensagem:\n").lower()
 deslocamento = int(input("Digite o número de deslocamento:\n"))
-if deslocamento > 26:
-    deslocamento = deslocamento % 26 
-    #se o número de deslocamento for maior que 26, o resto da divisão por 26 será o número de deslocamento, ou seja 27 % 26 = 1
 
 def caesar(texto,deslocamento,direcao):
     texto_criptografado = ""
+    if direcao == "decodificar":
+        deslocamento *= -1
     for letra in texto:
         posicao = alfabeto.index(letra)
         nova_posicao = posicao + deslocamento
-        if direcao == "decodificar":
-            nova_posicao -= 2*deslocamento
         texto_criptografado += alfabeto[nova_posicao]
     print(f"O texto '{texto}' {direcao} é: {texto_criptografado}")
 
