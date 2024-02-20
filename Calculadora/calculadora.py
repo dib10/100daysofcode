52850b888d352e14d9ec3b0c7bb118289601f65a
dib10 <caiodib10@gmail.com>
2024-02-20 13:10:13 -0300

    inverti a  exibição de n2 com o simbolo

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -19,12 +19,12 @@ def div(n1,n2):
 operacao = {'+': soma, '-':sub, '*':mult, '/':div}
 
 num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
 #imprimir as operações
 print("Digite a operação desejada: ")
 for i in operacao:
     print(i)
 simbolo = input("Digite o simbolo da operação: ")   
+num2 = int(input("Digite o segundo número: "))
 calcula = operacao[simbolo]
 resposta = calcula(num1,num2)
 print(f"{num1} {simbolo} {num2} = {resposta}")
