36ace0067bbde058d6d3d4465e383ee514adef6a
dib10 <caiodib10@gmail.com>
2024-02-20 13:02:19 -0300

    calculadora

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,26 @@
+#Calculadora
+
+#soma
+def soma(n1,n2):
+    return n1 + n2
+
+#subtração
+def sub(n1,n2):
+    return n1 - n2
+
+#multiplicação
+def mult(n1,n2):
+    return n1 * n2
+
+#divisão
+def div(n1,n2):
+    return n1 / n2
+
+operacao = {'+': soma, '-':sub, '*':mult, '/':div}
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+#imprimir as operações
+print("Digite a operação desejada: ")
+for i in operacao:
+    print(i)
