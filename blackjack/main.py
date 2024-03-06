a421f3c90de58b97744038e42bfff05716ae8424
dib10 <caiodib10@gmail.com>
2024-03-06 16:52:38 -0300

    adicionando funções e reorganizando código/melhorando a lógica

--- blackjack/main.py
+++ blackjack/main.py
@@ -42,43 +42,41 @@
 #Dica 14: Pergunte ao usuário se ele quer reiniciar o jogo. Se eles responderem sim, limpe o console e comece um novo jogo de blackjack e mostre o logotipo de art.py.
 
 
-from random import choice
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
 
-def deal_card():
-    return choice(cards)
-#distribuindo cards
-user_cards = []
-computer_cards = []
-for i in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
+def deal_card(): 
+    '''Retorna uma carta aleatória do baralho'''
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
 def calculate_score(cards):
+    '''Pega uma lista de cartas e retorna a pontuação calculada'''
     if sum(cards) == 21 and len(cards) == 2:
         return 0
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
     return sum(cards)
+#distribuindo cards
+user_cards = []
+computer_cards = []
+is_game_over = False
+for i in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+user_score = calculate_score(user_cards)
+print(f" Suas cartas: {user_cards}, pontuação: {user_score}")
+print(f" Cartas do computador: {computer_cards[0]}") #para mostrar apenas a primeira carta do computador
+computer_score = calculate_score(computer_cards)
 
-def compare(user_score, computer_score):
-    if user_score == computer_score:
-        return "Empate"
-    elif computer_score ==0:
-        return "Você perdeu, o computador tem um Blackjack"
-    elif user_score == 0:
-        return "Você ganhou com um Blackjack"
-    elif user_score > 21:
-        return "Você perdeu, você ultrapassou 21"
-    elif computer_score > 21:
-        return "Você ganhou, o computador ultrapassou 21"
-    elif user_score > computer_score:
-        return "Você ganhou"
-    else:
-        return "Você perdeu"
 
-    
+
+
+
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
 
 
 
