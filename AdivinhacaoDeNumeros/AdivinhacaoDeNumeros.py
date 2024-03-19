9bcae973032a20fc926ef2a1dbf1a0e050185fda
dib10 <caiodib10@gmail.com>
2024-03-19 12:25:22 -0300

    iniciando o projeto do dia

---
+++ AdivinhacaoDeNumeros/AdivinhacaoDeNumeros.py
@@ -0,0 +1,9 @@
+# Objetivos do Jogo de Adivinhação de Números:
+
+# Incluir um logotipo em arte ASCII.
+# Permitir que o jogador envie um palpite para um número entre 1 e 100.
+# Verificar o palpite do usuário contra a resposta real. Imprimir "Muito alto." ou "Muito baixo." dependendo da resposta do usuário.
+# Se o jogador acertar a resposta, mostrar a resposta real ao jogador.
+# Rastrear o número de turnos restantes.
+# Se eles ficarem sem turnos, fornecer feedback ao jogador.
+# Incluir dois níveis de dificuldade diferentes (por exemplo, 10 tentativas no modo fácil, apenas 5 tentativas no modo difícil).
