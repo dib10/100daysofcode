f087cc91323996cb67a02aafa4c8709684ab45c3
dib10 <caiodib10@gmail.com>
2024-03-18 21:08:41 -0300

    demonstração aula

--- AdivinhacaoDeNumeros/day-12-start.py
+++ AdivinhacaoDeNumeros/day-12-start.py
@@ -1,26 +1,33 @@
 ################### Scope ####################
 
-enemies = 1
+#enemies = 1
 
-def increase_enemies():
-  enemies = 2
-  print(f"enemies inside function: {enemies}")
+#ef increase_enemies():
+ # enemies = 2
+ # print(f"enemies inside function: {enemies}")
 
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+#increase_enemies()
+#print(f"enemies outside function: {enemies}")
 
 #local scope
 
 
 #global scope
-player_health = 10
+#player_health = 10
 
-def drink_potion():
-  potion_strength = 2
-  print(potion_strength)
-  print(player_health)
+#def drink_potion():
+  #potion_strength = 2
+  #print(potion_strength)
+ # print(player_health)
 
-drink_potion()
+#drink_potion()
+def create_enemy():
+    game_level = 3
+    enemies =["Skeleton","Zombie","Alien"]
+    if game_level:
+        new_enemy= enemies[0]
+
+    print(new_enemy)
 
 
 
