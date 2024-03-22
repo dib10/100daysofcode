dde963c1b32b81171a5b63a092f1581b65c682da
dib10 <caiodib10@gmail.com>
2024-03-22 19:41:49 -0300

    modifiquei exbicao final de erro, ele mostra numeros para comprovar a resposta

--- MaisMenos/main.py
+++ MaisMenos/main.py
@@ -2,19 +2,18 @@ from dados import data
 from art import logo
 import os
 import random
+
 def limpar_terminal():
     os.system('cls')  # 'cls' é o comando para limpar o terminal no Windows
+
 def selecionar_dois_itens():
     itens_selecionados = random.sample(data, 2) #sample pega 2 itens aleatorios da lista data
     return itens_selecionados
-
-A, B = selecionar_dois_itens()
 
 def exibir_itens(A,B):
     print(f"A: {A['name']}, {A['description']}, {A['country']}")
     print(logo)
     print(f"B: {B['name']}, {B['description']}, {B['country']}")
-
 
 def comparar_seguidores(A,B):
     seguidores_A = A['follower_count']
@@ -28,20 +27,27 @@ def jogo():
     pontos = 0
     continuar = True
     while continuar:
-        limpar_terminal()
         A,B = selecionar_dois_itens()
         exibir_itens(A,B)
         resposta = input("Quem tem mais seguidores? Digite 'A' ou 'B':").upper()
         resultado = comparar_seguidores(A,B)
+
+        if resultado == 'A':
+            resultado_dict = A
+        else:
+            resultado_dict = B
+
+        if resposta == 'A':
+            resposta_dict = A
+        else:
+            resposta_dict = B
+
         if resposta == resultado:
             pontos+=1
             print(f"Você acertou! Pontuação: {pontos}")
         else:
             print(f"Você errou! Pontuação final: {pontos}")
+            print(f"O correto era {resultado_dict['name']} com {resultado_dict['follower_count']} seguidores, você escolheu {resposta_dict['name']} com {resposta_dict['follower_count']} seguidores")
             continuar = False
+
 jogo()
-
-
-
-    
-
