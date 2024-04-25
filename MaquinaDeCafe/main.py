432fe4f367dba712360edf59dfe7e68b008698f2
dib10 <caiodib10@gmail.com>
2024-04-24 21:45:41 -0300

    dicionário com  ingredientes

--- MaquinaDeCafe/main.py
+++ MaquinaDeCafe/main.py
@@ -2,3 +2,36 @@ from  art import logo
 
 print("Bem vindo(a) a máquina de café!")
 print(logo)
+
+MENU = {
+    "espresso": {
+        "ingredientes": {
+            "água": 50,
+            "café": 18,
+        },
+        "custo": 1.5,
+    },
+    "latte": {
+        "ingredientes": {
+            "água": 200,
+            "leite": 150,
+            "café": 24,
+        },
+        "custo": 2.5,
+    },
+    "cappuccino": {
+        "ingredientes": {
+            "água": 250,
+            "leite": 100,
+            "café": 24,
+        },
+        "custo": 3.0,
+    }
+}
+
+recursos = {
+    "água": 300,
+    "leite": 200,
+    "café": 100,
+}
+
