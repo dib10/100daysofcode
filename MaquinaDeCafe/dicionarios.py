17dd81e0b48d6874afac6a188cda5eb3eb05f5c0
dib10 <caiodib10@gmail.com>
2024-04-25 15:10:10 -0300

    primeira implementacao da cafeteira funcionando

---
+++ MaquinaDeCafe/dicionarios.py
@@ -0,0 +1,31 @@
+MENU = {
+    "espresso": {
+        "ingredientes": {
+            "água": 50,
+            "café": 18,
+        },
+        "custo": 1.5,
+    },
+    "latte": {
+        "ingredientes": {
+            "água": 200,
+            "leite": 150,
+            "café": 24,
+        },
+        "custo": 2.5,
+    },
+    "cappuccino": {
+        "ingredientes": {
+            "água": 250,
+            "leite": 100,
+            "café": 24,
+        },
+        "custo": 3.0,
+    }
+}
+
+recursos = {
+    "água": 300,
+    "leite": 200,
+    "café": 100,
+}
--- MaquinaDeCafe/main.py
+++ MaquinaDeCafe/main.py
@@ -1,47 +1,25 @@
 from  art import logo
+from dicionarios import MENU,recursos
+import os
 
-print("Bem vindo(a) a máquina de café! ☕️")
-print(logo)
-
-MENU = {
-    "espresso": {
-        "ingredientes": {
-            "água": 50,
-            "café": 18,
-        },
-        "custo": 1.5,
-    },
-    "latte": {
-        "ingredientes": {
-            "água": 200,
-            "leite": 150,
-            "café": 24,
-        },
-        "custo": 2.5,
-    },
-    "cappuccino": {
-        "ingredientes": {
-            "água": 250,
-            "leite": 100,
-            "café": 24,
-        },
-        "custo": 3.0,
-    }
-}
-
-recursos = {
-    "água": 300,
-    "leite": 200,
-    "café": 100,
-}
+print("Recursos disponíveis:")
+for item in recursos:
+    print(f"{item}: {recursos[item]}ml")
 
 def exibir_sabores():
     for i, sabor in enumerate(MENU,start=1): #sabor é o que itera sobre o dicionário MENU
         print(f"{i}. {sabor} - \033[92mR${MENU[sabor]['custo']}\033[0m")
-    int(input("Digite o número correspondente ao café desejado:"))
-exibir_sabores()
+    escolha = int(input("Digite o número correspondente ao café desejado:"))
+    if escolha == 1:
+        return "espresso"
+    elif escolha == 2:
+        return "latte"
+    elif escolha == 3:
+        return "cappuccino"
+    else:
+        print("Opção inválida.")
 
-def verificar_moedas():
+def inserir():
     moedas = {
         'Quarters': 0.25,
         'Dimes': 0.10,
@@ -53,4 +31,51 @@ def verificar_moedas():
         total_moedas += int(input(f"Quantas moedas de {moeda} você deseja inserir?")) * moedas[moeda]
     return total_moedas
 
+def verifica_recursos(sabor):
+    for item in MENU[sabor]['ingredientes']:
+        if MENU[sabor]['ingredientes'][item] > recursos[item]:
+            print(f"Desculpe, não há ingredientes suficientes para fazer o café {sabor}.")
+            return False
+    return True
+        
+def processar(sabor,total_moedas):
+    custo =MENU[sabor]['custo']
+    if total_moedas >=custo:
+        for item in MENU[sabor]['ingredientes']:
+            recursos[item] -=MENU[sabor]['ingredientes'][item]
+            print(f"Você selecionou {sabor}, que custa R${custo}")
+            print(f"Você inseriu {round(total_moedas, 2)}.")
+            print(f"Troco: R${round(total_moedas - custo,2)}")
+            return True
+    else:
+        print("Desculpe, não há dinheiro suficiente.")
+        return False
+    
+def limpar_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+#programa principal
+def main():
+    print("Bem vindo(a) a máquina de café! ☕️")
+    print(logo)
+    exibir_relatório = input("Deseja ver o relatório de recursos? [1] para sim, [2] para não: ")
+    if exibir_relatório == '1':
+        print("Recursos disponíveis:")
+        for item in recursos:
+            print(f"{item}: {recursos[item]}ml")
+    sabor = exibir_sabores()
+    if verifica_recursos(sabor):
+        total_moedas = inserir()
+        if processar(sabor,total_moedas):
+            print(f"Aqui está o seu: {sabor} ☕️. Aproveite!")
+    continuar = input("Deseja fazer outro café? aperte [1] para sim, e [2] para não: ")
+    if continuar == '2':
+        return
+    else:
+        limpar_terminal()
+
+# Loop principal do programa
+while True:
+    limpar_terminal()
+    main()
+        
