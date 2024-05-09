a32a0c07fa949d917e36d9393abf3770b48d7efd
dib10 <caiodib10@gmail.com>
2024-05-09 16:01:33 -0300

    criando objeto menu

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -2,8 +2,14 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+menu = Menu()
+
 money_machine = MoneyMachine()
 money_machine.report()
 
 coffe_maker = CoffeeMaker()
 coffe_maker.report()
+
+is_on = True
+
+while is_on:
