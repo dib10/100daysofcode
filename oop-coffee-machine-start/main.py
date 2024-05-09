f6a973af9f046544cbaee6db7ed3f98a9bc8eb16
dib10 <caiodib10@gmail.com>
2024-05-09 16:22:19 -0300

    resolucao do exercicio da cafeteira com poo

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -7,9 +7,21 @@ menu = Menu()
 money_machine = MoneyMachine()
 money_machine.report()
 
-coffe_maker = CoffeeMaker()
-coffe_maker.report()
+coffee_maker = CoffeeMaker()
+coffee_maker.report()
 
 is_on = True
 
 while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like?({options}): ")
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if drink is not None:
+            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
