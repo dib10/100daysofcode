e634e3deb4203ed07146b7c26c0b28adfdd2b4b9
dib10 <caiodib10@gmail.com>
2024-05-13 18:09:21 -0300

    seguidores e seguindo

--- Quizz/main.py
+++ Quizz/main.py
@@ -3,8 +3,17 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+    def follou(self,user):
+        user.followers += 1
+        self.following += 1
 user_1 = User("001", "Caio")
 user_2 = User("002", "João")
 
-print(user_1.username) # Caio
-print(user_2.username) # João
+
+user_1.follou(user_2)
+print(user_1.followers)
+print(user_1.following)
+
+print(user_2.followers)
+print(user_2.following)
