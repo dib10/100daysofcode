39d4d354ab38d95ab5be23762699031cd564ade4
dib10 <caiodib10@gmail.com>
2024-05-29 19:57:39 -0300

    desafiio

--- Quizz/main.py
+++ Quizz/main.py
@@ -1,19 +1,11 @@
-class User:
-    def __init__(self,user_id,username):
-        self.id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
-    def follou(self,user):
-        user.followers += 1
-        self.following += 1
-user_1 = User("001", "Caio")
-user_2 = User("002", "João")
+from question_model import Question
+from data import question_data
 
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text,question_text)
+    question_bank.append(new_question)
 
-user_1.follou(user_2)
-print(user_1.followers)
-print(user_1.following)
-
-print(user_2.followers)
-print(user_2.following)
+print(question_bank[0].answer)
