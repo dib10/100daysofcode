6d35b301222e210f42fc1a31032a6ce4bfa6ef07
Caio <caiodib10@gmail.com>
2024-07-22 20:58:12 -0300

    parte 1 projeto
    parte 1 projeto

---
+++ Sessao 18 -  Intermediate - Turtle & the Graphical User Interface (GUI)/hirst_painting/main.py
@@ -0,0 +1,13 @@
+# import colorgram
+# rgb_colors = [] 
+# # Specify the full path to the image file
+# colors = colorgram.extract('c:/Users/caio_/OneDrive/Área de Trabalho/100daysofcode/Sessao 18 -  Intermediate - Turtle & the Graphical User Interface (GUI)/hirst_painting/spot.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
+color_list = [(235, 234, 232), (240, 229, 235), (229, 238, 233), (225, 233, 238), (235, 37, 113), (143, 26, 67), (237, 72, 37), (220, 164, 55), (15, 140, 88), (174, 162, 50), (33, 122, 186), (52, 189, 227), (173, 44, 94), (242, 219, 57), (80, 24, 74), (127, 190, 92), (250, 223, 0), (23, 169, 122), (189, 67, 41), (207, 133, 165), (147, 29, 25), (35, 181, 208), (11, 101, 56), (239, 163, 193), (243, 166, 157), (164, 213, 180), (98, 8, 6), (140, 214, 231), (8, 70, 28), (50, 140, 208)]
